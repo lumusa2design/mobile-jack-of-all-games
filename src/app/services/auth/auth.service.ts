00e0e69add71890d63e02dc83bc4b6748de32ed0
AVR21 <alejandroviera12@gmail.com>
2025-05-13 18:00:58 +0100

    feat: added auth & game services.

---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, sendPasswordResetEmail, signInWithEmailAndPassword, signOut, user, User, UserCredential } from '@angular/fire/auth';
+import { from, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private auth: Auth) { }
+
+  getUser(): Observable<User | null> {
+    return user(this.auth);
+  }
+
+  login(email: string, password: string): Observable<UserCredential> {
+  return from(signInWithEmailAndPassword(this.auth, email, password));
+  }
+
+  signup(email: string, password: string): Observable<UserCredential> {
+    return from(createUserWithEmailAndPassword(this.auth, email, password));
+  }
+  
+  resetPassword(email: string): Observable<void> {
+    return from(sendPasswordResetEmail(this.auth, email));
+  }
+  
+  logout(): Observable<void> {
+    return from(signOut(this.auth));
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,34 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { getApp, initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, indexedDBLocalPersistence, initializeAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { Capacitor } from '@capacitor/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouter(routes, withPreloading(PreloadAllModules)), 
+    provideFirebaseApp(() => initializeApp({ 
+      projectId: "jack-of-all-games", 
+      appId: "1:831810578588:web:9ff12e4990757011976559", 
+      databaseURL: "https://jack-of-all-games-default-rtdb.europe-west1.firebasedatabase.app", 
+      storageBucket: "jack-of-all-games.firebasestorage.app", 
+      apiKey: "AIzaSyDcC73-4BIS4t4jFowahH3zamyhNiW9yZA", 
+      authDomain: "jack-of-all-games.firebaseapp.com", 
+      messagingSenderId: "831810578588", 
+      measurementId: "G-4G27BV4XSY" })), 
+      provideAuth(() => {
+        if (Capacitor.isNativePlatform()) {
+          return initializeAuth(getApp(), {
+            persistence: indexedDBLocalPersistence,
+          });
+        } else {
+          return getAuth();
+        }
+      }), 
+      provideFirestore(() => getFirestore()),
   ],
 });
