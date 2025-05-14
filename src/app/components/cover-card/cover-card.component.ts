8175f384128ae5be3d46ade7fb1b9c11a059f956
alvaro <alvaroruzab@gmail.com>
2025-05-14 12:23:51 +0100

    Cover-card

--- src/app/components/cover-card/cover-card.component.ts
+++ src/app/components/cover-card/cover-card.component.ts
@@ -1,11 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+//import {IonCard} from "@ionic/angular/standalone";
+import {IonicModule} from "@ionic/angular";
 
 @Component({
   selector: 'app-cover-card',
   templateUrl: './cover-card.component.html',
   styleUrls: ['./cover-card.component.scss'],
+  standalone: true,
+  imports: [
+      IonCard,
+      IonicModule
+  ]
 })
 export class CoverCardComponent  implements OnInit {
+
+  @Input() title!: string;
+  @Input() imgPath!: string;
 
   constructor() { }
 
