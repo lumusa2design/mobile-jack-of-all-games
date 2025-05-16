a430d6b14477608ec5363ceb04aa7b248f81bdbb
AVR21 <alejandroviera12@gmail.com>
2025-05-16 21:16:22 +0100

    feat: SQL CRUD with local database.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { DatabaseService } from './services/database/database.service';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,12 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private database: DatabaseService) {
+    this.initDB();
+  }
+
+  async initDB() {
+    await this.database.initialize();
+  }
+
 }
---
+++ src/app/services/database/database.service.ts
@@ -0,0 +1,77 @@
+import { Injectable, signal, WritableSignal } from '@angular/core';
+import { CapacitorSQLite, SQLiteConnection, SQLiteDBConnection } from '@capacitor-community/sqlite';
+import { Game } from 'src/app/models/game';
+
+const DB_NAME = 'favouritegames'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatabaseService {
+
+  private sqlite: SQLiteConnection = new SQLiteConnection(CapacitorSQLite);
+  private db!: SQLiteDBConnection
+  private gameList: WritableSignal<Game[]> = signal([]);
+
+  constructor() { }
+
+  async initialize() {
+    this.db = await this.sqlite.createConnection(
+      DB_NAME,
+      false,
+      'no-encryption',
+      1,
+      false
+    );
+
+    await this.db.open();
+
+    const schema = `CREATE TABLE IF NOT EXISTS favourites (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      gameId TEXT NOT NULL
+      );`;
+
+    await this.db.execute(schema);
+    this.loadGames();
+    return true;
+  }
+
+  async loadGames() {
+    const games = await this.db.query('SELECT * FROM favourites;');
+    this.gameList.set(games.values || []);
+  }
+
+  async addGame(id: string) {
+    const query = `INSERT INTO favourites (gameId) VALUES (${id})`;
+    const result = await this.db.query(query);
+
+    this.loadGames();
+    
+    return result;
+  }
+
+  getGameList() {
+    return this.gameList;
+  }
+
+  /*
+  async updateGameById(id: string, newValue: string) {
+    const query = `UPDATE favourites SET value=${newValue} WHERE id=${id}`;
+    const result = await this.db.query(query);
+
+    this.loadGames();
+
+    return result;
+  }
+  */
+
+  async deleteGame(id: string) {
+    const query = `DELETE FROM favourites WHERE id=${id}`;
+    const result = await this.db.query(query);
+
+    this.loadGames();
+
+    return result;
+  }
+
+}
