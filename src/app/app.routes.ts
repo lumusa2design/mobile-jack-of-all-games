14bd709167dbe1f36aec66b66c96a6bfc4093340
Luis Muñoz Sanz <lumusa2design@gmail.com>
2025-05-20 18:11:22 +0100

    Feat: mandatory login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,19 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: 'identify',
     loadComponent: () => import('./pages/identifier-page/identifier-page.component').then((m) => m.IdentifierPageComponent),
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'identify',
     pathMatch: 'full',
   },
   {
     path: '**',
-    redirectTo: 'home',
+    redirectTo: 'identify',
   }
 ];
