4be3a403602da8b19fca140c8a426429f0c0c94d
alvaro <alvaroruzab@gmail.com>
2025-05-20 18:20:23 +0100

    Added: Library con enlaces funcionales a sus game-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,16 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/identifier-page/identifier-page.component').then((m) => m.IdentifierPageComponent),
   },
   {
+    path: 'game/:id',
+    loadComponent: () => import('./pages/game-page/game-page.component').then((m) => m.GamePageComponent),
+  },
+  {
+    path: 'library',
+    loadComponent: () => import('./pages/library/library.component').then((m) => m.LibraryPageComponent),
+  },
+  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'library',
     pathMatch: 'full',
   },
   {
--- src/app/components/cover-card/cover-card.component.ts
+++ src/app/components/cover-card/cover-card.component.ts
@@ -1,14 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonImg,
+  IonCardContent
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-cover-card',
+  standalone: true,
   templateUrl: './cover-card.component.html',
   styleUrls: ['./cover-card.component.scss'],
+  imports: [
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonImg,
+    IonCardContent
+  ]
 })
-export class CoverCardComponent  implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class CoverCardComponent {
+  @Input() title!: string;
+  @Input() imgPath!: string;
 }
